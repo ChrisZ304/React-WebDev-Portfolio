bed53df2958c9ef4cb16dec1c6af06cf10bf080d
Chris Z <c.zacharias2021@gmail.com>
2021-09-09 19:41:47 -0400

    style sections

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,9 @@ import "./contact.scss"
 
 export default function Contact() {
   return (
-    <div className ="contact" id="contact "></div>
+    <div className ="contact" id="contact ">
+      contact
+    </div>
   )
   
 };
